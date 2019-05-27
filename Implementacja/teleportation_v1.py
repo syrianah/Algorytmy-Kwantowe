e2db479e895b4012841b3002a6a2236667aca7af
Wojciech Kubiak <syrianahminecraft@gmail.com>
2019-05-27 19:45:59 +0200

    teleportation

--- Implementacja/teleportation_v1.py
+++ Implementacja/teleportation_v1.py
@@ -1,4 +1,4 @@
-from qubit import Qubit, tensordot, Hadamard, Cnot, Measure, Identity, RCnot
+from qubit import Qubit, tensordot, Hadamard, Cnot, Measure, Identity, RCnot, randomQ
 from complex import Complex
 import numpy as np
 import math
@@ -16,37 +16,46 @@ y = Qubit(Complex(1, 0), Complex(0, 0))
 x = x * H
 xy = tensordot(x, y)
 xy = np.tensordot(CNOT, xy, axes=[1,0])
-print(xy)
+print("splątany = ", xy)
 
 #Qubit do teleportowania
-psi = Qubit(Complex(0,0), Complex(1,0))
-# print(psi.alpha, psi.beta)
+psi = randomQ()
+# psi = Qubit(Complex(0, 0), Complex(1, 0))
+print("psi = ", psi.alpha, psi.beta)
 
 #Pierwszy Krok
-first = np.kron(xy, psi.vector())
-# print(first)
+first = np.kron(psi.vector(), xy)
+print("pierwszy = ", first)
 
 #Drugi Krok
 Cnot = np.kron(CNOT, I)
 second = np.tensordot(Cnot, first, axes=[1,0])
-# print(second)
+print("drugi = ", second)
 
 #Trzeci Krok
 H = np.kron(H, I)
 H = np.kron(H, I)
 # print(H)
 third = np.tensordot(H, second, axes=[1,0])
-print(third)
+print("trzeci = ", third)
 
 #Pomiar
 M = np.array([[Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
             [Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
+            [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
             [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
             [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-            [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-            [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-            [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-            [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)]])
+            [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0)],
+            [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0)],
+            [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)]])
 
-P = np.tensordot(M, third, axes=[1,0])
-print(P)
+# P = np.tensordot(third, third, axes=[1,0])
+# print(P)
+P = np.dot(third, third)
+# print(P)
+
+# P = (1 - abs(P)**2)/2
+# print("wynik", P)
+
+# q1 = Qubit(third[0], third[1])
+# print(q1.alpha, q1.beta)
