3c152b4e61972c929f75252727361d95b55e9b74
BuildTools <unconfigured@null.spigotmc.org>
2020-06-22 00:35:45 +0200

    add rotate gates and chsh game

--- Implementacja/qubit.py
+++ Implementacja/qubit.py
@@ -5,40 +5,49 @@ import random
 from complex import Complex, PolarToComplex
 from vector import Vector
 
+
 def P0(qubit):
     return abs(qubit.alpha)**2
+
 
 def P1(qubit):
     return abs(qubit.beta)**2
 
+
 def Qubit_after_measure(qubit):
-    if P0(qubit)>P1(qubit):
-        alfa=qubit.alpha/abs(qubit.alpha)
-        return Qubit(alfa,Complex(0,0))
+    if P0(qubit) > P1(qubit):
+        alfa = qubit.alpha/abs(qubit.alpha)
+        return Qubit(alfa, Complex(0, 0))
     else:
         beta = qubit.beta/abs(qubit.beta)
-        return Qubit(Complex(0,0), beta)
+        return Qubit(Complex(0, 0), beta)
+
 
 def Measure(qubit):
     if P0(qubit) > P1(qubit):
         P = random.uniform(0, 1)
         if P < P0(qubit):
             return 0
-        else: return 1
+        else:
+            return 1
     elif P0(qubit) < P1(qubit):
         P = random.uniform(0, 1)
         if P < P1(qubit):
             return 1
-        else: return 0
+        else:
+            return 0
     else:
         P = random.uniform(0, 1)
         if P < 0.5:
             return 0
-        else: return 1
+        else:
+            return 1
+
 
 def tensordot(a, b):
     return np.kron(a.vector(), b.vector())
     # return np.tensordot(a.vector(), b.vector(), axes=0)
+
 
 def randomQ():
     a = random.uniform(-1, 1)
@@ -58,72 +67,126 @@ def randomQ():
     beta = Complex(c, d)
     return Qubit(alpha, beta)
 
+
 def M0():
     return np.array([[Complex(1, 0), Complex(0, 0)],
-            [Complex(0, 0), Complex(0, 0)]])
+                     [Complex(0, 0), Complex(0, 0)]])
+
 
 def M1():
     return np.array([[Complex(0, 0), Complex(0, 0)],
-            [Complex(0, 0), Complex(1, 0)]])
+                     [Complex(0, 0), Complex(1, 0)]])
 
 
 def Identity():
-    return np.array([[Complex(1,0), Complex(0,0)],
-                    [Complex(0,0), Complex(1,0)]])
+    return np.array([[Complex(1, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(1, 0)]])
+
 
 def Hadamard():
     return np.array([[Complex(1/np.sqrt(2), 0), Complex(1/np.sqrt(2), 0)],
-                    [Complex(1/np.sqrt(2), 0), Complex(-1/np.sqrt(2), 0)]])
+                     [Complex(1/np.sqrt(2), 0), Complex(-1/np.sqrt(2), 0)]])
+
 
 def PauliX():
     return np.array([[Complex(0, 0), Complex(1, 0)],
-                    [Complex(1, 0), Complex(0, 0)]])
+                     [Complex(1, 0), Complex(0, 0)]])
+
 
 def PauliY():
     return np.array([[Complex(0, 0), Complex(0, -1)],
-                    [Complex(0, 1), Complex(0, 0)]])
+                     [Complex(0, 1), Complex(0, 0)]])
+
 
 def PauliZ():
     return np.array([[Complex(1, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(-1, 0)]])
+                     [Complex(0, 0), Complex(-1, 0)]])
+
 
 def Sgate():
     return np.array([[Complex(1, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 1)]])
+                     [Complex(0, 0), Complex(0, 1)]])
+
+
 def SNgate():
     return np.array([[Complex(1, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, -1)]])
+                     [Complex(0, 0), Complex(0, -1)]])
+
 
 def Tgate():
     return np.array([[Complex(1, 0), Complex(0, 0)],
-                    [Complex(0, 0), PolarToComplex(1, -45)]])
+                     [Complex(0, 0), PolarToComplex(1, -45)]])
+
 
 def TNgate():
     return np.array([[Complex(1, 0), Complex(0, 0)],
-                    [Complex(0, 0), PolarToComplex(1, 45)]])
+                     [Complex(0, 0), PolarToComplex(1, 45)]])
+
 
 def Cnot():
     return np.array([[Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0)]])
+                     [Complex(0, 0), Complex(1, 0),
+                      Complex(0, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(0, 0),
+                      Complex(0, 0), Complex(1, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0)]])
+
 
 def RCnot():
     return np.array([[Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0)]])
+                     [Complex(0, 0), Complex(0, 0),
+                      Complex(0, 0), Complex(1, 0)],
+                     [Complex(0, 0), Complex(0, 0),
+                      Complex(1, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0)]])
+
+
+def RX(qubit, phi):
+    gate = np.array([[Complex(math.cos(phi/2), 0), Complex(0, math.sin(phi/2))],
+                     [Complex(0, math.sin(phi/2)), Complex(math.cos(phi/2), 0)]])
+
+    alpha = gate[0, 0] * qubit.alpha + gate[0, 1] * qubit.beta
+    beta = gate[1, 0] * qubit.alpha + gate[1, 1] * qubit.beta
+    qubit.update_state(alpha, beta)
+    return Qubit(qubit.alpha, qubit.beta)
+
+
+def RY(phi):
+    return np.array([[Complex(math.cos(phi/2), 0), Complex(0, math.sin(phi/2)*1)],
+                     [Complex(0, math.sin(phi/2)*(-1)), Complex(math.cos(phi/2), 0)]])
+
+    # alpha = gate[0, 0] * qubit.alpha + gate[0, 1] * qubit.beta
+    # beta = gate[1, 0] * qubit.alpha + gate[1, 1] * qubit.beta
+    # qubit.update_state(alpha, beta)
+    # return Qubit(qubit.alpha, qubit.beta)
+
+
+def RZ(qubit, phi):
+    gate = np.array([[Complex(math.cos(phi/2), math.sin(phi/2)), Complex(0, 0)],
+                     [Complex(0, 0), Complex(math.cos(phi/2), math.sin(phi/2)*(-1))]])
+
+    alpha = gate[0, 0] * qubit.alpha + gate[0, 1] * qubit.beta
+    beta = gate[1, 0] * qubit.alpha + gate[1, 1] * qubit.beta
+    qubit.update_state(alpha, beta)
+    return Qubit(qubit.alpha, qubit.beta)
+
 
 def Toffoli():
     return np.array([[Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0)],
-                    [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0)]])
-    
+                     [Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0), Complex(
+                         0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0), Complex(
+                         0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(
+                         0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(
+                         1, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(
+                         0, 0), Complex(1, 0), Complex(0, 0), Complex(0, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(
+                         0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0)],
+                     [Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(0, 0), Complex(1, 0), Complex(0, 0)]])
+
 
 class Qubit:
     def __init__(self, alpha, beta):
@@ -141,11 +204,12 @@ class Qubit:
     def vector(self):
         return [self.alpha, self.beta]
 
+
 m1 = np.array([[Complex(1, 0), Complex(0, 0)],
-                [Complex(0, 0), Complex(0, 0)]])
+               [Complex(0, 0), Complex(0, 0)]])
 
 m2 = np.array([[Complex(0, 0), Complex(0, 0)],
-                [Complex(0, 0), Complex(1, 0)]])
+               [Complex(0, 0), Complex(1, 0)]])
 
 # print(0.4677056485736148 + 0.5322943514263851)
 # a = Complex(1/np.sqrt(2), 0)
@@ -180,15 +244,15 @@ CNOT = Cnot()
 RCNOT = RCnot()
 # print(RCNOT)
 
-#Odwrotny CNOT
+# Odwrotny CNOT
 Hdot = np.kron(H, H)
 # print(Hdot)
 # print(CNOT)
 
-HdotCnot = np.tensordot(CNOT, Hdot, axes=[0,1])
+HdotCnot = np.tensordot(CNOT, Hdot, axes=[0, 1])
 # print(HdotCnot)
 
-ReverseCnot = np.tensordot(HdotCnot, Hdot, axes=[0,1])
+ReverseCnot = np.tensordot(HdotCnot, Hdot, axes=[0, 1])
 # print(ReverseCnot)
 
 # new_qX = q1 * PX
@@ -237,4 +301,3 @@ ReverseCnot = np.tensordot(HdotCnot, Hdot, axes=[0,1])
 # ReverseCnot = np.tensordot(HdotCnot, Hdot, axes=[0,1])
 
 # print(ReverseCnot)
-
